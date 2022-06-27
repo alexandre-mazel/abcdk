555f5263572a2564b09ef6320f09524b0ae06f19
Alexander Mazel <amazel@aldebaran.com>
2022-06-27 09:30:10 +0200

    first push

---
+++ README.md
@@ -0,0 +1,1 @@
+Our own abcdk with cool modifications
---
+++ push_pull_all_repos.sh
@@ -0,0 +1,23 @@
+# todo: redo it automatically enumerating sub folders
+
+echo Pull Push All repos !
+
+git pull; git push
+cd ../choregraphe_boxes
+git pull;git push
+cd ../demos
+git pull;git push
+cd ../scripts
+git pull;git push
+cd ../apps
+git pull;git push
+cd ../tutorials
+git pull;git push
+cd ../choregraphe_boxes
+git pull;git push
+cd ../datas
+git pull;git push
+cd ../services
+git pull;git push
+
+cd ../abcdk
